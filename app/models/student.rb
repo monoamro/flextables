dc4ce964ac241620e0b5eaf0e5f7b921c13b4b1f
(Andreas Alexandropoulos) andreas.alexandropoulos@posteo.de <andreas.alexandropoulos@posteo.de>
2020-06-02 18:29:17 +0200

    add validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,6 @@ class Student < ApplicationRecord
   belongs_to :user
   belongs_to :grade
   has_many :time_slots
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true, uniqueness: true
 end
