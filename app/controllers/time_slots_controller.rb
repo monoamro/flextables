ea90b018a51e6182986edb9c30d4fe8204b4511a
Malte Kerssenfischer <malte.kerssenfischer@gmail.com>
2020-06-04 18:38:26 +0200

    dynamic timetable

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -2,7 +2,11 @@ class TimeSlotsController < ApplicationController
 
   def index
     @time_slots = current_user.students.first.time_slots
-    @spots = (1..20).to_a
+    @monday_spots    = [1, 6, 11, 16]
+    @tuesday_spots   = [2, 7, 12, 17]
+    @wednesday_spots = [3, 8, 13, 18]
+    @thursday_spots  = [4, 9, 14, 19]
+    @friday_spots    = [5, 10, 15, 20]
   end
 
 end
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -3,4 +3,58 @@ class TimeSlot < ApplicationRecord
   belongs_to :lesson
   validates :weekly_period, uniqueness: { scope: :student_id,
                                           message: "This Slot is already full" }
+
+TRANSLATE_HASH_start = {
+  1 => '8:00',
+  2 => '8:00',
+  3 => '8:00',
+  4 => '8:00',
+  5 => '8:00',
+  6 => '9:00',
+  7 => '9:00',
+  8 => '9:00',
+  9 => '9:00',
+  10 => '9:00',
+  11 => '10:00',
+  12 => '10:00',
+  13 => '10:00',
+  14 => '10:00',
+  15 => '10:00',
+  16 => '11:00',
+  17 => '11:00',
+  18 => '11:00',
+  19 => '11:00',
+  20 => '11:00'
+}
+
+TRANSLATE_HASH_end = {
+  1 => '8:45',
+  2 => '8:45',
+  3 => '8:45',
+  4 => '8:45',
+  5 => '8:45',
+  6 => '9:45',
+  7 => '9:45',
+  8 => '9:45',
+  9 => '9:45',
+  10 => '9:45',
+  11 => '10:45',
+  12 => '10:45',
+  13 => '10:45',
+  14 => '10:45',
+  15 => '10:45',
+  16 => '11:45',
+  17 => '11:45',
+  18 => '11:45',
+  19 => '11:45',
+  20 => '11:45'
+}
+
+  def starttime
+    TRANSLATE_HASH_start(weekly_period)
+  end
+
+  def endtime
+    TRANSLATE_HASH_start(weekly_period)
+  end
 end
