b5d80e2308a8823793a62ccdd4376e5cb8273110
Montaser Amro <mono.amro@gmail.com>
2020-06-05 16:55:38 +0200

    JS fixes

--- app/javascript/codepen/schedule.js
+++ app/javascript/codepen/schedule.js
@@ -118,11 +118,13 @@ const schedule = () => {
     this.modalHeader.find('.event-date').text(event.find('.event-date').text());
     this.modal.attr('data-event', event.parent().attr('data-event'));
 
-    //update event content
-    // this.modalBody.find('.event-info').load(event.parent().attr('data-content')+'.html .event-info > *', function(data){
-    //   //once the event content has been loaded
-    //   self.element.addClass('content-loaded');
-    // });
+    // update event content
+    this.modalBody.find('.event-info').load(event.parent().attr('data-content')+'.html .event-info > *', function(data){
+      //once the event content has been loaded
+      self.element.addClass('content-loaded');
+      const showPartial = jQuery.parseHTML(data)[14];
+      self.element[0].children[2].children[1].children[0].appendChild(showPartial);
+    });
 
     this.element.addClass('modal-is-open');
 
