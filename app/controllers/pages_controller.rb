b45c145ca5bf3edb8ccb3048a5485169d61d4ff1
Esther Bernasconi <est.bernasconi+anmeldung@gmail.com>
2020-06-11 16:02:34 +0200

    update impressum and about, linked to footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :impressum, :about ]
 
   def home
+  end
+
+  def impressum
+  end
+
+  def about
   end
 end
